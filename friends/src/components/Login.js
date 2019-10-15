95aa79dc99a76f4db695c2c7af3b6be792191987
Francis Bulus <notchera@gmail.com>
2019-10-15 22:30:20 +0100

    built out login component with event handlers implemented

---
+++ friends/src/components/Login.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+const initialFormState = {
+  username: "",
+  password: ""
+};
+
+export default function Login(props) {
+  const [loginInfo, setLoginInfo] = useState(initialFormState);
+
+  const handleChange = e =>
+    setLoginInfo({
+      ...loginInfo,
+      [e.target.name]: e.target.value
+    });
+
+  const submitHandler = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/login", loginInfo)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/friendsdisplay");
+      })
+      .catch(error => console.log(error));
+  };
+
+  if (localStorage.getItem("token")) return <Redirect to="/friendsdisplay" />;
+
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          name="username"
+          placeholder="enter username"
+          autoComplete="off"
+          value={loginInfo.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="enter password"
+          autoComplete="off"
+          value={loginInfo.password}
+          onChange={handleChange}
+        />
+        <button>Log in</button>
+      </form>
+    </div>
+  );
+}
