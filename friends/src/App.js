09a5bf9dc6d18c199f0e1329ad0fdf009f369199
Francis Bulus <notchera@gmail.com>
2019-10-15 22:19:43 +0100

    built out App component rendering structure

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import Display from "./components/Display";
+import Login from "./components/Login";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/friendsdisplay">My Friends</Link>
+          </li>
+          <li>
+            <Link to="/login">Log In</Link>
+          </li>
+        </ul>
+      </div>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <PrivateRoute path="/friendsdisplay" component={Display} />
+      </Switch>
+    </Router>
   );
 }
 
