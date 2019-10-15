c64a5b4c8886a33b35006f77a966d9f3f45c45c7
Francis Bulus <notchera@gmail.com>
2019-10-15 22:53:39 +0100

    built out display component

---
+++ friends/src/components/Display.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddFriend from './AddFriend';
+import Friends from './Friends';
+
+export default function Display() {
+  return (
+    <div>
+      <AddFriend />
+      <Friends />
+    </div>
+  );
+}
